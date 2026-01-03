fc189cdf2785561a49ea1b53cb77f9ed1198189b
thekey78 <thekey78@gmail.com>
2026-01-03 15:09:26 +0900

    Initialize Spring Boot project with Maven wrapper configuration and basic project setup.

---
+++ src/main/java/pe/kr/thekey/framework/ThekeyFrameworkApplication.java
@@ -0,0 +1,13 @@
+package pe.kr.thekey.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThekeyFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThekeyFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/kr/thekey/framework/ThekeyFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.kr.thekey.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThekeyFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
